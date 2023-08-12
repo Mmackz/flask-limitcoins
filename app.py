b95aba7111178c83749e339c2bbfe669be86e842
Mmackz <matthewmackenz@gmail.com>
2023-08-12 12:58:45 -0700

    🔊 add logging

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
 from modules import authenticate, update
 from waitress import serve
+import logging
+
+# Logging configuration
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 reddit = authenticate()
 app = Flask(__name__)
@@ -11,10 +16,15 @@ def index():
 
 @app.route("/update_data", methods=["POST"])
 def update_data():
-    update(reddit)
-    return "Data updated successfully"
+    try:
+        update(reddit)
+        logging.info('Data updated successfully')
+        return "Data updated successfully"
+    except Exception as e:
+        logging.error(f"Error updating data: {str(e)}")
+        return f"Error: {str(e)}", 500
   
 if __name__ == "__main__":
-#   app.run("0.0.0.0",port=8081)
+    logging.info("Starting server...")
     serve(app)
 
